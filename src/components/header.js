acd9179f5a6f42add2cd1a62ed2eed6875b662f3
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-05 18:54:08 +0100

    Mobile header done

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,87 @@
 import React from "react"
 import styled from "@emotion/styled"
 import { css } from "@emotion/core"
+import { Link } from "gatsby"
+import Logo from "../images/logo.svg"
 const StyledHeader = styled.header`
-  color: ${({ theme }) => theme.colors.primary.green};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
+
+const StyledLogo = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const StyledMobileNavBtn = styled.button`
+  background: none;
+  border: none;
+  height: 3rem;
+  width: 3rem;
+  position: relative;
+  padding: 0;
+  display: flex;
+  
+  flex-direction: column;
+  justify-content: space-around;
+  outline:none;
+  span {
+    width: 100%;
+    height: 1px;
+    border-radius: 20px;
+    background: ${({ theme }) => theme.colors.primary.blue};
+    transition:all .5s;
+    transform-origin:1px;
+
+${({ active }) =>
+  active &&
+  css`
+    :nth-child(1) {
+      
+      transform:rotate(45deg)
+    }
+    :nth-child(2) {
+      opacity:0;
+    }
+    :nth-child(3) {
+      transform:rotate(-45deg)
+
+    }
+  }
+`}
+   
+`
+const StyledNav = styled.nav`
+  display: none;
+`
+const StyledNavItem = styled(Link)``
+const StyledCta = styled.div`
+  display: none;
+`
+
 export default function Header() {
-  return <StyledHeader>Header</StyledHeader>
+  const [menuActive, setMenuActive] = React.useState(false)
+  return (
+    <StyledHeader>
+      <StyledLogo>
+        <img src={Logo} alt="" />
+      </StyledLogo>
+      <StyledMobileNavBtn
+        active={menuActive}
+        onClick={() => setMenuActive(!menuActive)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </StyledMobileNavBtn>
+      <StyledNav>
+        <StyledNavItem to="/">Home</StyledNavItem>
+        <StyledNavItem to="/about">About</StyledNavItem>
+        <StyledNavItem to="/contact">Contact</StyledNavItem>
+        <StyledNavItem to="/blog">Blog</StyledNavItem>
+        <StyledNavItem to="/careers">Careers</StyledNavItem>
+      </StyledNav>
+    </StyledHeader>
+  )
 }
