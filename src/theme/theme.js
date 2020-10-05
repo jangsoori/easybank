52a0e92e903d360bf19f7dd00a47519dfe3c59b8
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-05 18:53:52 +0100

    Updated theming

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -8,5 +8,11 @@ export const theme = {
       green: "hsl(136, 65%, 51%)",
       cyan: "hsl(192, 70%, 51%)",
     },
+    neutral: {
+      grayBlue: "hsl(233, 8%, 62%)",
+      lightGrayBlue: "hsl(220, 16%, 96%)",
+      vLightGray: "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+    },
   },
 }
