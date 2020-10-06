042673ef45041d3a49ff21785779745cb90e5c38
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-06 16:22:56 +0100

    Popup menu styles

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,13 @@ const StyledHeader = styled.header`
   align-items: center;
   padding: 2rem;
   z-index: 100;
+  position: relative;
   background: ${({ theme }) => theme.colors.neutral.white};
+  ${({ mobileActive, theme }) =>
+    mobileActive &&
+    css`
+      box-shadow: 0px 50px 300px 600px hsla(233, 26%, 24%, 0.7);
+    `}
 `
 
 const StyledLogo = styled.div`
@@ -57,6 +63,23 @@ ${({ active }) =>
 `
 const StyledNav = styled.nav`
   display: none;
+  ${({ mobileActive }) =>
+    mobileActive &&
+    css`
+      padding: 2rem 0;
+      background: white;
+      position: absolute;
+      display: grid;
+      grid-template-rows: repeat(5, max-content);
+      text-align: center;
+      left: 50%;
+      top: 9rem;
+      transform: translateX(-50%);
+      border-radius: 5px;
+      gap: 2.4rem;
+      font-size: 1.8rem;
+      width: 32rem;
+    `}
 `
 const StyledNavItem = styled(Link)``
 const StyledCta = styled.div`
@@ -66,7 +89,7 @@ const StyledCta = styled.div`
 export default function Header() {
   const [menuActive, setMenuActive] = React.useState(false)
   return (
-    <StyledHeader>
+    <StyledHeader mobileActive={menuActive}>
       <StyledLogo>
         <Logo />
       </StyledLogo>
@@ -78,7 +101,7 @@ export default function Header() {
         <span></span>
         <span></span>
       </StyledMobileNavBtn>
-      <StyledNav>
+      <StyledNav mobileActive={menuActive}>
         <StyledNavItem to="/">Home</StyledNavItem>
         <StyledNavItem to="/about">About</StyledNavItem>
         <StyledNavItem to="/contact">Contact</StyledNavItem>
