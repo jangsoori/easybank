a9e3a78ca3bb034b76986c980373cf23dba4954c
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-06 10:19:22 +0100

    Reusable section

---
+++ src/styled/section.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled"
+
+export const Section = styled.div`
+  display: grid;
+  row-gap: 2.5rem;
+  grid-auto-rows: max-content;
+  padding: 0 2rem;
+
+  justify-content: center;
+  text-align: center;
+`
