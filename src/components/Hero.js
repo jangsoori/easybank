8497e8454d771e6d480f337f5e2d10ca3f1fcc2c
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-06 15:09:09 +0100

    Finished hero section (mobile)

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,18 @@
 import React from "react"
 import styled from "@emotion/styled"
 import { css } from "@emotion/core"
-import background from "../images/bg-intro-mobile.svg"
-import mockups from "../images/image-mockups.png"
+import background from "../assets/svg/bg-intro-mobile.svg"
+import mockups from "../assets/image-mockups.png"
 import { BodyText } from "../styled/bodyText"
 import { BodyTitle } from "../styled/bodyTitle"
 import { Section } from "../styled/section"
 import CtaBtn from "./CtaBtn"
-const StyledHeroSection = styled.section``
+const StyledHeroSection = styled.section`
+  padding-bottom: 8rem;
+`
 const MockupsContainer = styled.div`
   position: relative;
-  height: max-content;
+  height: 30%;
 `
 const Background = styled.img`
   margin-top: -3rem;
@@ -30,7 +32,9 @@ export default function Hero() {
         <Background src={background} alt="" />
         <Mockups src={mockups} alt="" />
       </MockupsContainer>
-      <Section style={{ marginTop: "-4rem" }}>
+      <Section
+        style={{ background: "white", marginTop: "-4rem", padding: "0 2rem" }}
+      >
         <BodyTitle size="4.5rem">
           Next generation
           <br />
