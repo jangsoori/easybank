f90d3559753baabe1f4bee09f3f3da01b555c072
jangsoori <46868329+jangsoori@users.noreply.github.com>
2020-10-07 11:47:25 +0100

    Added gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
+const path = require(`path`)
+
 module.exports = {
   plugins: [
     `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+        ignore: `svg`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
